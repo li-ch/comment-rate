234032e61857d04e85d5f20d3cf784e56f237da0
Li Chen <li-ch@users.noreply.github.com>
2016-11-02 14:24:24 +0800

    Update do.sh
    put the intermediate file to /tmp

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # input: src directory of code
 
-find $1 \( -name "*.h" -or -name "*.cpp" \) -exec cat {} \; > all_code.c
-./comment-rate.sh all_code.c
+find $1 \( -name "*.h" -or -name "*.cpp" \) -exec cat {} \; > /tmp/all_code.c
+./comment-rate.sh /tmp/all_code.c
 
